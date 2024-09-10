16ce9442c743d5661db7c34f4b128bc5e8cfb815
Manjuatgithub25 <manjunathyashram5@gmail.com>
2024-09-10 12:00:15 +0530

    updated

--- test/test_runner.py
+++ test/test_runner.py
@@ -1,8 +1,6 @@
 import pytest
 from requests import request
 from page_objects.test_login import LoginLogout
-from page_objects.test_my_info import MyInfo
-from page_objects.test_recruitment import Recruitment
 
 
 @pytest.mark.usefixtures("setup")
@@ -14,9 +12,7 @@ class Test:
         user_management.add_user_credentials()
         leave_page = user_management.delete_user_from_users_list()
         Recruitment_page = leave_page.input_leave_details()
-        Recruitment_page = Recruitment(self.driver)
-        my_info_page = Recruitment_page.input_recruitment_details
-        # my_info_page = MyInfo(self.driver)
+        my_info_page = Recruitment_page.input_recruitment_details()
         my_info_page.input_personal_details()
         my_info_page.add_attachment()
         login_logout.logout()
